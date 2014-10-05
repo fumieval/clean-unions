07fbb5d2ef576995fba9011e52c64f5f4f0d9153
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-10-05 11:48:12 +0900

    rename

--- src/Data/OpenUnion1/Clean.hs
+++ src/Data/OpenUnion1/Clean.hs
@@ -43,8 +43,6 @@ instance (Functor f, Functor (Union s)) => Functor (Union (f :> s)) where
   fmap f (Single m) = Single (fmap f m)
   fmap f (Union u) = Union (fmap f u)
 
-
-
 -- | Perform type-safe matching.
 (||>) :: (f x -> r) -- ^ first case
   -> (Union s x -> r) -- ^ otherwise
@@ -59,26 +57,26 @@ exhaust (Exhausted a) = exhaust a
 simply :: (f a -> r) -> ((f |> Nil) a -> r)
 simply f = f ||> exhaust
 
-data MemberInfo f s where
-  Head :: MemberInfo f (f :> s)
-  Tail :: MemberInfo f s -> MemberInfo f (g :> s)
+data Position f s where
+  Zero :: Position f (f :> s)
+  Succ :: Position f s -> Position f (g :> s)
 
 -- | Constraint @f ∈ s@ indicates that @f@ is an element of a type-level list @s@.
 class f ∈ s | s -> f where
-  query :: Proxy f -> Proxy s -> MemberInfo f s
+  query :: Proxy f -> Proxy s -> Position f s
 
 infix 4 ∈
 infix 4 ⊆
 
-instance f ∈ (f :> s) where query _ _ = Head
-instance (f ∈ s) => f ∈ (g :> s) where query p q = Tail (query p (Proxy :: Proxy s))
+instance f ∈ (f :> s) where query _ _ = Zero
+instance (f ∈ s) => f ∈ (g :> s) where query p q = Succ (query p (Proxy :: Proxy s))
 
 -- | Lift some value into a union.
 liftU :: forall s f a. (f ∈ s) => f a -> Union s a
 liftU f = go $ query (Proxy :: Proxy f) (Proxy :: Proxy s) where
-  go :: forall t. MemberInfo f t -> Union t a
-  go Head = Single f
-  go (Tail q) = Union (go q)
+  go :: forall t. Position f t -> Union t a
+  go Zero = Single f
+  go (Succ q) = Union (go q)
 
 -- | Type-level inclusion characterized by 'reunion'.
 class s ⊆ t where
